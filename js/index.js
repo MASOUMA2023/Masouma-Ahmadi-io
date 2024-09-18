af0b04a1fd701b7ccb1d50de3b5fb4c132bde8ae
MASOUMA2023 <ahmadimasooma12@gmail.com>
2024-09-17 21:16:53 -0400

    Portfolio Project Rubric

--- js/index.js
+++ js/index.js
@@ -1,85 +1,101 @@
-const footer = document.createElement('footer');
-const body = document.querySelector('body');
+//add footer
+const footer = document.createElement("footer");
+const body = document.querySelector("body");
 body.appendChild(footer);
-
 
 const today = new Date();
 const year = today.getFullYear();
-const copyright = document.createElement('p');
+const copyright = document.createElement("p");
 copyright.innerHTML = `<span>Masouma</span><span>&#169</span><span>${year}</span><span>`;
 footer.appendChild(copyright);
 
-const skillsList = ['JavaScript','HTML','CSS','IT support','Cybersecurity & Risk Management'];
-const skillsSection = document.getElementById('skills');
-const skillsUL = skillsSection.querySelector('ul');
+//list the skills under skills section
+const skillsList = [
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "IT support",
+  "Cybersecurity & Risk Management",
+];
+const skillsSection = document.getElementById("skills");
+const skillsUL = skillsSection.querySelector("ul");
 
-for (let skill of skillsList){
-      let skillItem = document.createElement('li');
-      skillItem.innerHTML = skill;
-      skillsUL.appendChild(skillItem);
-
+for (let skill of skillsList) {
+  let skillItem = document.createElement("li");
+  skillItem.innerHTML = skill;
+  skillsUL.appendChild(skillItem);
 }
 
+
+//create message form under message section
 const messageForm = document.getElementById("leave_message");
 
 messageForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const userName = e.target.usersName.value;
-    const email = e.target.usersEmail.value;
-    const message = e.target.usersMessage.value;
+  const userName = e.target.usersName.value;
+  const email = e.target.usersEmail.value;
+  const message = e.target.usersMessage.value;
 
-    console.log('userName:', userName);
-    console.log('email:' , email);
-    console.log('message:' ,message);
-   
-    const messageList = document.getElementById('messages_list');
-    const messagesSection = document.getElementById('messages');
-    const listItem = document.createElement('li');
-    listItem.classList.add('message_item');
+  console.log("userName:", userName);
+  console.log("email:", email);
+  console.log("message:", message);
 
-   listItem.innerHTML =  `<a href="mailto:${email} "> ${userName} </a>
+  //add messages list under the form
+  const messageList = document.getElementById("messages_list");
+  const messagesSection = document.getElementById("messages");
+  const listItem = document.createElement("li");
+  listItem.classList.add("message_item");
+
+  listItem.innerHTML = `<a href="mailto:${email} "> ${userName} </a>
    <span>wrote:${message}</span>`;
-    listItem.appendChild(makeEditButton());
-    listItem.appendChild(makeRemoveButton());
-    messageList.appendChild(listItem);
-    messagesSection.hidden = false;
+  listItem.appendChild(makeEditButton());
+  listItem.appendChild(makeRemoveButton());
+  messageList.appendChild(listItem);
+  messagesSection.hidden = false;
 
-    messageForm.reset();
+  messageForm.reset();
 });
-   
-function makeEditButton(){
-    const button =document.createElement('button');
-    button.textContent = 'Edit';
-    button.type = 'button';
-    button.classList.add('edit-button');
-    button.addEventListener('click', () => {
-        const entry = button.parentNode;;
-        const messageSpan = entry.querySelector('span');
-        const newMessage = prompt('Edit your message:', messageSpan.textContent.replace('wrote: ', ''));
-        if (newMessage) {
-            messageSpan.textContent = `wrote: ${newMessage}`;
-        }
-    });
-    return button;
+
+//create edit button for message section
+function makeEditButton() {
+  const button = document.createElement("button");
+  button.textContent = "Edit";
+  button.type = "button";
+  button.classList.add("edit-button");
+  button.addEventListener("click", () => {
+    const entry = button.parentNode;
+    const messageSpan = entry.querySelector("span");
+    const newMessage = prompt(
+      "Edit your message:",
+      messageSpan.textContent.replace("wrote: ", "")
+    );
+    if (newMessage) {
+      messageSpan.textContent = `wrote: ${newMessage}`;
+    }
+  });
+  return button;
 }
 
+//create remove button for message section
 function makeRemoveButton() {
-    const button = document.createElement('button');
-    button.textContent = 'Remove';
-    button.type ='button';
-    button.classList.add('remove-button');
-    button.addEventListener('click', () => {
-        const entry = button.parentNode;
-        entry.remove();
-        // Hide messages section if no messages are left
-        if (messageList.children.length === 0) {
-            messagesSection.hidden = true;
-        };
-    });
-    return button;
-} 
-    
+  const button = document.createElement("button");
+  button.textContent = "Remove";
+  button.type = "button";
+  button.classList.add("remove-button");
+  button.addEventListener("click", () => {
+    const entry = button.parentNode;
+    entry.remove();
+    // Hide messages section if no messages are left
+    if (messageList.children.length === 0) {
+      messagesSection.hidden = true;
+    } else messagesSection.hidden = false;
+  });
+  return button;
+}
+
+
+//fetch repositories list from gitHub
 const userName = "MASOUMA2023";
 fetch(`https://api.github.com/users/${userName}/repos`)
   .then((response) => {
@@ -93,6 +109,7 @@ fetch(`https://api.github.com/users/${userName}/repos`)
     const repositories = data;
     console.log(repositories);
 
+    //list repos under projects section
     const projectSection = document.getElementById("projects");
     const projectList = document.createElement("ul");
     projectSection.appendChild(projectList);
@@ -106,5 +123,3 @@ fetch(`https://api.github.com/users/${userName}/repos`)
   .catch((error) => {
     console.error("Error:", error);
   });
-    
-        
